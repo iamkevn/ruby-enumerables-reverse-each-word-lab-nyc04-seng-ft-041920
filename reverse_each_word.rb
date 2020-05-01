b77ba00f8e9ee22dddf061159c741f05e5d26f2e
Kevin Rodriguez <iamkevnrodriguez@gmail.com>
2020-05-01 13:58:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,8 +8,8 @@ end
 
 
 
-#def reverse_each_word (sen)
-#  array = %w[sen]
-#  array.collect { |word| word.reverse! }
-#  array.join(" ")
-#end
+def reverse_each_word (sen)
+  array = sen.split(" ")
+  array.collect { |word| word.reverse! }
+  array.join(" ")
+end
