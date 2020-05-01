5f10c3e10f6615ddef5f50fd28c4e98f8b28c4a9
Kevin Rodriguez <iamkevnrodriguez@gmail.com>
2020-05-01 13:46:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,15 @@
 
 
-def reverse_each_word (str)
-  array = %w[str]
+def reverse_each_word (sen)
+  array = %w[sen]
   array.each { |word| word.reverse! }
   array.join(" ")
 end
 
 
 
-#def reverse_each_word ("Hi again, just making sure it's reversed!")
-#  array = %w["Hi again, just making sure it's reversed!"]
+#def reverse_each_word (sen)
+#  array = %w[sen]
 #  array.collect { |word| word.reverse! }
 #  array.join(" ")
 #end
